db5339320cd2edb730fdc565492f741ae2f8b490
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-19 16:55:08 +0300

    Using minio as blob storage

--- interface/settings.py
+++ interface/settings.py
@@ -8,3 +8,9 @@ DEBUG = True
 _hostname = os.environ.get('HOSTNAME')
 if _hostname:
     ALLOWED_HOSTS = [_hostname]
+
+VMCK_API_URL = os.environ.get('VMCK_API_URL', 'http://localhost:8000')
+
+MINIO_ADDRESS = os.environ.get('MINIO_ADDRESS', 'localhost:9000')
+MINIO_ACCESS_KEY = os.environ.get('MINIO_ACCESS_KEY', "changeme")
+MINIO_SECRET_KEY = os.environ.get('MINIO_SECRET_KEY', "changemetoo")
--- interface/views.py
+++ interface/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .forms import UploadFileForm
+from .submission import handle_submission
 
 
 def homepage(request):
     if request.method == 'POST'and request.FILES:
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            print('success')
+            handle_submission(request.FILES['file'])
     else:
         form = UploadFileForm()
 
