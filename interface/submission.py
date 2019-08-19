f4f2c685480703aa977f44c239ef563635e89be1
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-19 17:50:09 +0300

    Minio API

--- interface/submission.py
+++ interface/submission.py
@@ -1,31 +1,24 @@
 import logging
-import io
 from django.conf import settings
-from minio import Minio
-from minio.error import ResponseError
-from .utils import random_code
-from zipfile import ZipFile
 from tempfile import TemporaryDirectory
+from .minio_api import MinioAPI
+from zipfile import ZipFile
+from pathlib import Path
+from shutil import copy
 
 log_level = logging.DEBUG
 log = logging.getLogger(__name__)
 log.setLevel(log_level)
 
 
-def save(file):
-    minioClient = Minio(settings.MINIO_ADDRESS,
-                        access_key=settings.MINIO_ACCESS_KEY,
-                        secret_key=settings.MINIO_SECRET_KEY,
-                        secure=False)
-
-    minioClient.put_object('test',
-                           file.name,
-                           io.BytesIO(file.read()),
-                           file.size,
-                           content_type='application/zip')
-
-
 def handle_submission(file):
     log.debug(f'Submission {file.name} received')
+    storage =  MinioAPI.getInstance()
 
-    save(file)
+    storage.upload(file)
+    with TemporaryDirectory() as _tmp:
+        tmp = Path(str(_tmp))
+        log.debug(f'Temporary directory at {tmp}')
+        storage.download(file.name, tmp / 'archive.zip')
+        copy(settings.base_dir / 'vagrant' / 'Vagrantfile', tmp)
+
