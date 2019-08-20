1a6164ac1b3cb422a2c54ff355b17eee8a8492b2
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-20 12:39:07 +0300

    Mock login form

--- interface/forms.py
+++ interface/forms.py
@@ -4,3 +4,8 @@ from django import forms
 class UploadFileForm(forms.Form):
     file = forms.FileField(label='Select archive',
                            widget=forms.FileInput(attrs={'accept': 'application/zip'}))  # noqa: E501
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label='Username')
+    password = forms.CharField(label='Password', widget=forms.PasswordInput)
--- interface/views.py
+++ interface/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
-from .forms import UploadFileForm
+from django.shortcuts import render, redirect
+from .forms import UploadFileForm, LoginForm
 from .submission import handle_submission
 
 
 def homepage(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return redirect(upload)
+    else:
+        form = LoginForm()
+
+    return render(request, 'interface/homepage.html', {'form': form})
+
+
+def upload(request):
     if request.method == 'POST'and request.FILES:
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -11,4 +22,4 @@ def homepage(request):
     else:
         form = UploadFileForm()
 
-    return render(request, 'interface/homepage.html', {'form': form})
+    return render(request, 'interface/upload.html', {'form': form})
