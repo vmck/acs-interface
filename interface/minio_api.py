50d84ae713ac49eb82e25430a59a62ed894dd6d9
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-21 19:16:42 +0300

    Review changes

--- interface/minio_api.py
+++ interface/minio_api.py
@@ -3,33 +3,22 @@ from minio import Minio
 from django.conf import settings
 
 
-class MinioAPI:
-    __instance = None
+_client = Minio(settings.MINIO_ADDRESS,
+                access_key=settings.MINIO_ACCESS_KEY,
+                secret_key=settings.MINIO_SECRET_KEY,
+                secure=False)
 
-    @staticmethod
-    def getInstance():
-        if MinioAPI.__instance is None:
-            MinioAPI()
 
-        return MinioAPI.__instance
+def upload(file):
+    _client.put_object('test',
+                       file.name,
+                       io.BytesIO(file.read()),
+                       file.size,
+                       content_type='application/zip')
 
-    def __init__(self):
-        MinioAPI.__instance = self
 
-        self.client = Minio(settings.MINIO_ADDRESS,
-                            access_key=settings.MINIO_ACCESS_KEY,
-                            secret_key=settings.MINIO_SECRET_KEY,
-                            secure=False)
-
-    def upload(self, file):
-        self.client.put_object('test',
-                               file.name,
-                               io.BytesIO(file.read()),
-                               file.size,
-                               content_type='application/zip')
-
-    def download(self, filename, path):
-        with open(path, 'wb') as file:
-            data = self.client.get_object('test', filename)
-            for chunck in data.stream(32*1024):
-                file.write(chunck)
+def download(filename, path):
+    with open(path, 'wb') as file:
+        data = _client.get_object('test', filename)
+        for chunck in data.stream(32*1024):
+            file.write(chunck)
