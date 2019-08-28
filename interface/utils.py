d13e8a067cfcd500db0cdae91629e56d26e98073
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-29 01:12:39 +0300

    migartions & clean-up

--- interface/utils.py
+++ interface/utils.py
@@ -14,3 +14,8 @@ def is_number(string):
 
 def random_code(length, vocabulary=vocabulary_64):
     return ''.join(secrets.choice(vocabulary) for _ in range(length))
+
+
+def is_true(value):
+    text = (value or '').lower().strip()
+    return text in ['1', 'yes', 'true', 'on', 'enabled']
