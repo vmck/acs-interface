94bbe64ce720ef9aa6ad81b9991f4ffadd256098
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-08-29 23:31:29 +0300

    exempt api from csrf check

--- assignment/pc.sh
+++ assignment/pc.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ls -al
+clear
+echo "You failed miserably..."
+echo "Here is your error: Bad code"
+echo 0/100
--- interface/views.py
+++ interface/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from interface.forms import UploadFileForm, LoginForm
 from interface.backend.submission import handle_submission
+from interface.forms import UploadFileForm, LoginForm
+from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from interface import models
 
@@ -35,17 +36,20 @@ def upload(request):
     return render(request, 'interface/upload.html', {'form': form})
 
 
+@csrf_exempt
 def done(request):
     # NOTE: make it safe, some form of authentication
     #       we don't want stundets updating their score.
-    options = json.loads(request.body) if request.body else {}  # TODO validate
+    options = json.loads(request.body, strict=False) if request.body else {}  # noqa: E501
 
-    submission = get_object_or_404(models.Submission, id=options['id'])
+    submission = get_object_or_404(models.Submission, url=options['token'])
     submission.score = 0
 
     log.debug(f'Submission #{submission.id} has the output:\n{options["output"]}')  # noqa: E501
 
     submission.save()
+
+    return JsonResponse({})
 
 
 def alive(request):
