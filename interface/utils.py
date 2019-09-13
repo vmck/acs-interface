b30c8c7ad7ffc89a8759dce430029aa5d91cc4ed
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-13 16:12:17 +0300

    Add django pagination

--- interface/utils.py
+++ interface/utils.py
@@ -6,26 +6,6 @@ import secrets
 vocabulary_64 = string.ascii_letters + string.digits + '.+'
 
 
-def get_table_next_prev(submissions, base_url, page):
-    next_url = ''
-    prev_url = ''
-
-    if submissions:
-        first_id = submissions[0].id
-
-        if first_id > settings.SUBMISSIONS_PER_PAGE:
-            next_url = base_url + f'?page={page + 1}'
-        else:
-            next_url = base_url + f'?page={page}'
-
-        if page == 1:
-            prev_url = base_url + f'?page={page}'
-        else:
-            prev_url = base_url + f'?page={page - 1}'
-
-    return (next_url, prev_url)
-
-
 def is_number(string):
     try:
         int(string)
