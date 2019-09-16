26c0ab1f46116edabbe003447bfeae94d0e91d50
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-16 14:54:54 +0300

    Add vmck_url

--- ci/set-up.sh
+++ ci/set-up.sh
@@ -9,7 +9,8 @@ docker run --detach --restart always \
   --env NOMAD_URL=http://10.42.1.1:4646 \
   --env DEBUG=true \
   --env BACKEND=qemu \
-  --env QEMU_IMAGE_PATH_PREFIX=http://10.42.1.1:10001\
+  --env QEMU_IMAGE_PATH_PREFIX=http://10.42.1.1:10001 \
   --env QEMU_CPU_MHZ=1000 \
+  --env VMCK_URL=http://127.0.0.1:8000 \
   --publish 127.0.0.1:8000:8000 \
   vmck/vmck:api-update
