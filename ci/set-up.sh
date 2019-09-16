d2a030c5f291c51ede932270be462609a478d6fa
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-16 14:47:22 +0300

    Use docker

--- ci/set-up.sh
+++ ci/set-up.sh
@@ -1,23 +1,15 @@
 #!/bin/bash -ex
 
-cd /opt
-git clone https://github.com/vmck/vmck.git
-cd vmck
-git checkout api-update
-
-echo "SECRET_KEY=TODO:ChangeME!!!" >> .env
-echo "HOSTNAME=*" >> .env
-echo "SSH_USERNAME=vagrant" >> .env
-echo "CONSUL_URL=http://10.42.1.1:8500" >> .env
-echo "NOMAD_URL=http://10.42.1.1:4646" >> .env
-echo "VMCK_URL=http://localhost:8000" >> .env
-echo "BACKEND=qemu" >> .env
-echo "QEMU_CPU_MHZ=3000" >> .env
-echo "QEMU_IMAGE_PATH_PREFIX=http://10.42.1.1:10001" >> .env
-
-mkdir data
-touch data/db.sqlite3
-
-sudo pipenv install
-sudo pipenv run ./manage.py migrate
-sudo nohup pipenv run python ./server.py
+docker run --detach --restart always \
+  --name vmck \
+  --volume /opt/volumes/vmck/data:/opt/vmck/data \
+  --env SECRET_KEY=TODO:ChangeME!!! \
+  --env HOSTNAME=* \
+  --env CONSUL_URL=http://10.42.1.1:8500 \
+  --env NOMAD_URL=http://10.42.1.1:4646 \
+  --env DEBUG=true \
+  --env BACKEND=qemu \
+  --env QEMU_IMAGE_PATH_PREFIX=http://10.42.1.1:10001\
+  --env QEMU_CPU_MHZ=1000 \
+  --publish 127.0.0.1:8000:8000 \
+  vmck/vmck:api-update
