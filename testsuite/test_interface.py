524819a5dd7ee92357b29f5a46e25fa43fdb2101
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-19 06:56:27 +0300

    Advanced pytest

--- testsuite/test_interface.py
+++ testsuite/test_interface.py
@@ -1,8 +1,11 @@
+import time
+
+import pytest
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
-import pytest
 
 import interface.backend.minio_api as storage
+from interface.models import Submission
 
 
 @pytest.mark.django_db
@@ -17,4 +20,17 @@ def test_submission(client):
                     data={'name': filepath.name, 'file': upload},
                     format='multipart')
 
+    assert len(Submission.objects.all()) == 1
     assert storage.exists('1.zip')
+
+    submission = Submission.objects.all()[0]
+
+    assert submission.state == submission.STATE_NEW
+    assert submission.assignment.code == 'pc-00'
+    assert submission.archive_size > 0
+    assert submission.vmck_job_id > 0
+
+    while submission.state != submission.STATE_DONE:
+        time.sleep(2)
+
+    assert submission.score > 0
