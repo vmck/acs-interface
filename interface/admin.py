b2dbd7a8326a888d6d706281524195cee3126980
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-19 15:48:53 +0300

    Get submissions in order

--- interface/admin.py
+++ interface/admin.py
@@ -3,6 +3,7 @@ from tempfile import TemporaryDirectory
 from pathlib import Path
 
 from django.contrib import admin
+from django.db.models import F
 from django.http import HttpResponse
 
 from interface.models import Course, Assignment, Submission
@@ -17,12 +18,9 @@ class AssignmentAdmin(admin.ModelAdmin):
 
     def download_submissions(self, request, _assignment):
         assignment = _assignment[0]
-        ta_user = request.user
-        ta_group = ta_user.groups.all()[0].name
 
         submissions = Submission.objects.filter(
-                            assignment=assignment,
-                            user__groups__name=ta_group).exclude(user=ta_user)
+                        assignment=assignment).order_by(F('timestamp').asc())
 
         with TemporaryDirectory() as _tmp:
             tmp = Path(_tmp)
@@ -30,8 +28,9 @@ class AssignmentAdmin(admin.ModelAdmin):
             with ZipFile(tmp / 'review.zip', 'x') as zipfile:
                 for submission in submissions:
                     submission.download(tmp / f'{submission.id}.zip')
-                    zipfile.write(tmp / f'{submission.id}.zip',
-                                  f'{submission.user.username}.zip')
+                    zipfile.write(
+                        tmp / f'{submission.id}.zip',
+                        f'{submission.user.username}-{submission.id}.zip')
 
             with open(tmp / 'review.zip', 'rb') as zipfile:
                 response = HttpResponse(zipfile.read(),
