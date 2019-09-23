7521b0d697ac77d61c7e68da2a1798d5d05dba5e
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-09-23 16:29:14 +0300

    Should look better

--- interface/admin.py
+++ interface/admin.py
@@ -3,7 +3,6 @@ from tempfile import TemporaryDirectory
 from pathlib import Path
 
 from django.contrib import admin
-from django.db.models import F
 from django.http import FileResponse
 
 from interface.models import Course, Assignment, Submission
@@ -16,34 +15,31 @@ admin.site.register(Submission)
 class AssignmentAdmin(admin.ModelAdmin):
     actions = ['download_submissions']
 
-    def download_submissions(self, request, _assignment):
-        if _assignment.count() != 1:
+    def download_submissions(self, request, queryset):
+        if queryset.count() != 1:
             raise RuntimeError('Only one assignment can be selected')
 
-        assignment = _assignment[0]
-
-        subs = Submission.objects.filter(
-                assignment=assignment).order_by(F('timestamp').asc())
+        assignment = queryset[0]
+        submission_set = assignment.submission_set.order_by('timestamp')
 
         submissions = {}
 
         # we only want the last submission of every user
-        for submission in subs:
+        for submission in submission_set:
             submissions[submission.user.username] = submission
 
         with TemporaryDirectory() as _tmp:
             tmp = Path(_tmp)
 
             with ZipFile(tmp / 'review.zip', 'x') as zipfile:
-                for _, submission in submissions.items():
+                for submission in submissions.values():
                     submission.download(tmp / f'{submission.id}.zip')
                     zipfile.write(
                         tmp / f'{submission.id}.zip',
-                        f'{submission.user.username}-{submission.id}.zip')
+                        f'{submission.user.username}-{submission.id}.zip'
+                        )
 
             review_zip = (tmp / 'review.zip').open('rb')
-            response = FileResponse(review_zip)
-
-            return response
+            return FileResponse(review_zip)
 
     download_submissions.short_description = 'Download submissions for review'
