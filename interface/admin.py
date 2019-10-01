ffa55f5d24919fdabc63f661d2b720cd4d1a4e26
Baruta Daniel Mihai <baruta.mihai@yahoo.com>
2019-10-01 17:55:57 +0300

    Download archives to review (#3)
    * Basic admin functionality
    
    * Get submission fro users that are in ta's group
    
    * Fix Flake8
    
    * Use zip
    
    * Don't exclude ta
    
    * Base django app
    
    * Using minio as blob storage
    
    * Minio API
    
    * Mock vagrant up
    
    * Mock login form
    
    * Flake8 fix
    
    * Readme
    
    * Bump flake8 version
    
    * Authentication system
    
    It will be changed when we have access to the uni's database
    
    * Review changes
    
    * Pytest
    
    * Upload submission with Vagrantfile to storage
    
    * Submission api
    
    * Toke for vm identification
    
    * Clean up and token fix
    
    * Clean-up & Submission model
    
    * Dockerfile
    
    * Deploy on cluster
    
    * migartions & clean-up
    
    * Send vmck url to job
    
    * exempt api from csrf check
    
    * Check submission output
    
    * Remove token
    
    * New nomad job name
    
    * New line for output
    
    * Blob storage in nomad job
    
    * Name mangling for archives
    
    * Call checker
    
    * Clean up & submissions list
    
    * Decode output with base64
    
    * Link to download archive
    
    * Upload submission button on submission list page
    
    * Prettify code
    
    * Model changes
    
    * Storage setup command
    
    * Get script from github
    
    * Get assignment info from repo
    
    * Show archive size
    
    * Server knows its ip address
    
    * Send address to vmck
    
    * State
    
    * Bootstraping
    
    * Fix text formatting
    
    * Cast json values
    
    * Submission options are in "env" key
    
    * Remove link expiry
    
    * Use jobs to create a submission
    
    * name to jobs
    
    * Remove vmck url from request
    
    * Add next/prev buttons
    
    * Add next/prev buttons
    
    * Review page
    
    * Remove double MINIO_ADDRESS
    
    * Clean up
    
    * Ignore checker files
    
    * add scroll
    
    * use id as token
    
    * See models in admin mode
    
    * Add static files for admin page
    
    * fix css style
    
    * Add submission to admin
    
    * Make it pretty :)
    
    * Use jq
    
    * fix
    
    * fix 2
    
    * Compress migrations and delete duplicate static
    
    * Don't know what I did!
    
    * Fix
    
    * Fix score
    
    * Fix
    
    * Remove static
    
    * Collect static in dockerfile
    
    * Remove review
    
    * Update address
    
    * Homepage
    
    * Fix flake8
    
    * Add django pagination
    
    * Update django navigation
    
    * Fix flake8
    
    * Fix username
    
    * Fix check
    
    * Reverse submission order
    
    * Basic admin functionality
    
    * Get submission fro users that are in ta's group
    
    * Fix Flake8
    
    * Use zip
    
    * Don't exclude ta
    
    * Revert "Reverse submission order"
    
    This reverts commit a39e3ee319e6c1b28c39bc0a1be43aa62b0b3e41.
    
    * Revert "Don't exclude ta"
    
    This reverts commit 389d651593e334c39a546f6614a840460bf3de4c.
    
    * Timestamp
    
    * Submission name
    
    * Get submissions in order
    
    * Works!
    
    * Review fix
    
    * Should look better
    
    * Add message
    
    * Fix
    
    * Use error
    
    * Better life

--- interface/admin.py
+++ interface/admin.py
@@ -1,6 +1,46 @@
-from django.contrib import admin
+from zipfile import ZipFile
+from tempfile import TemporaryDirectory
+from pathlib import Path
+
+from django.contrib import admin, messages
+from django.http import FileResponse
+
 from interface.models import Course, Assignment, Submission
 
 admin.site.register(Course)
 admin.site.register(Submission)
-admin.site.register(Assignment)
+
+
+@admin.register(Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+    actions = ['download_submissions']
+
+    def download_submissions(self, request, queryset):
+        if queryset.count() != 1:
+            messages.error(request, 'Only one assignment can be selected')
+            return
+
+        assignment = queryset[0]
+        submission_set = assignment.submission_set.order_by('timestamp')
+
+        submissions = {}
+
+        # we only want the last submission of every user
+        for submission in submission_set:
+            submissions[submission.user.username] = submission
+
+        with TemporaryDirectory() as _tmp:
+            tmp = Path(_tmp)
+
+            with ZipFile(tmp / 'review.zip', 'x') as zipfile:
+                for submission in submissions.values():
+                    submission.download(tmp / f'{submission.id}.zip')
+                    zipfile.write(
+                        tmp / f'{submission.id}.zip',
+                        f'{submission.user.username}-{submission.id}.zip',
+                    )
+
+            review_zip = (tmp / 'review.zip').open('rb')
+            return FileResponse(review_zip)
+
+    download_submissions.short_description = 'Download submissions for review'
