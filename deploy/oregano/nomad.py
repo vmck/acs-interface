0b4510117d9ff16d4a68b9b6a72fdbf09c97d1aa
Alex Morega <alex@grep.ro>
2019-10-02 18:44:43 +0300

    Nice object reprs

--- deploy/oregano/nomad.py
+++ deploy/oregano/nomad.py
@@ -8,6 +8,9 @@ class Provider:
     def __init__(self, nomad_addr):
         self.nomad_addr = nomad_addr
 
+    def __repr__(self):
+        return f"<nomad.Provider {self.nomad_addr!r}>"
+
     def render(self):
         return render('nomad_provider.tf', {
             'address': self.nomad_addr,
@@ -19,6 +22,9 @@ class Job:
     def __init__(self, name, template):
         self.name = name
         self.template = template
+
+    def __repr__(self):
+        return f"<nomad.Job {self.name!r}>"
 
     def render(self):
         return render('nomad_job.tf', {
--- deploy/tf/main.tf
+++ deploy/tf/main.tf
@@ -1,10 +1,10 @@
-# <oregano.nomad.Provider object at 0x10a399a50>
+# <nomad.Provider 'http://10.42.2.1:4646'>
 provider "nomad" {
   address = "http://10.42.2.1:4646"
   version = "~> 1.4"
 }
 
-# <oregano.nomad.Job object at 0x10a38ced0>
+# <nomad.Job 'acs-interface'>
 resource "nomad_job" "acs-interface" {
   jobspec = "${file("${path.module}/../interface.nomad")}"
 }
