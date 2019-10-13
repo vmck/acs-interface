202cf90cd138a46c09ea46fe8c9117cd4c54496c
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-10-13 14:27:47 +0300

    Revert state to new

--- interface/admin.py
+++ interface/admin.py
@@ -52,6 +52,7 @@ class SubmissionAdmin(admin.ModelAdmin):
 
     def rerun_submissions(self, request, submissions):
         for submission in submissions:
+            submission.state = Submission.STATE_NEW
             submission.evaluate()
 
     rerun_submissions.short_description = 'Re-run submissions'
