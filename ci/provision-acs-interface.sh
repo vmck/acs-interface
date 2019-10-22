0abe368ce9e979096b55438193cce65affb60a5c
Alex Morega <alex@grep.ro>
2019-10-22 14:24:49 +0300

    Start minio

--- ci/provision-acs-interface.sh
+++ ci/provision-acs-interface.sh
@@ -4,9 +4,17 @@ sudo apt-get update -qq
 sudo apt-get -qq install python3-pip libsasl2-dev python-dev libldap2-dev libssl-dev
 sudo pip3 install pipenv
 
+
 cd /vagrant
 pipenv install
 mkdir -p data
-pipenv run ./manage.py migrate
+
+container=$(docker ps -f name=minio -aq)
+if [ -z $container ]; then (
+  pipenv run examples/minio.sh
+) fi
+
+pipenv run python manage.py migrate
+pipenv run python manage.py storage_setup
 
 echo "✔ acs-interface installed successfully"
--- examples/minio.sh
+++ examples/minio.sh
@@ -2,8 +2,12 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
-docker run --rm -p 9000:9000 --name storage --detach \
+docker run --rm \
+  --restart always \
+  --name storage \
+  --detach \
   --volume $(pwd)/data/uploads:/data \
   --env "MINIO_ACCESS_KEY=${MINIO_ACCESS_KEY}" \
   --env "MINIO_SECRET_KEY=${MINIO_SECRET_KEY}" \
+  -p 9000:9000 \
   minio/minio server /data
