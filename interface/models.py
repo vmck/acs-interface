a05d768b78316373f2e01dff15a863615527c82d
Madalina Caea <mada.caea@gmail.com>
2019-10-22 16:13:22 +0300

    when an assignment is deleted, delete all its submissions

--- interface/models.py
+++ interface/models.py
@@ -68,7 +68,7 @@ class Submission(models.Model):
     ])
 
     assignment = models.ForeignKey(Assignment,
-                                   on_delete=models.PROTECT,
+                                   on_delete=models.CASCADE,
                                    null=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     output = models.CharField(max_length=32768, default='none')
