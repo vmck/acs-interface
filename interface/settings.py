28c0a20eab304483c621e8b8ad841782307d2b2e
Madalina Caea <mada.caea@gmail.com>
2019-10-22 13:38:53 +0300

    check if ldap is configured

--- interface/settings.py
+++ interface/settings.py
@@ -51,9 +51,11 @@ AUTH_LDAP_FIND_GROUP_PERMS = True
 AUTH_LDAP_CACHE_TIMEOUT = 3600
 
 AUTHENTICATION_BACKENDS = [
-    'django_auth_ldap.backend.LDAPBackend',
     "django.contrib.auth.backends.ModelBackend",
 ]
+
+if os.environ.get('LDAP_SERVER_URI'):
+	AUTHENTICATION_BACKENDS.insert(0, 'django_auth_ldap.backend.LDAPBackend')
 
 TEMPLATES = [
     {
