1c2b2e654a7e47a90016e97408dcb6170784e16f
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-12-16 00:11:02 +0200

    Pytest

--- ci/test-pytest.sh
+++ ci/test-pytest.sh
@@ -1,4 +1,15 @@
 #!/bin/bash -ex
 
 cd /vagrant
-exec pipenv run pytest
+
+echo "MINIO_ADDRESS=10.66.60.1:9000
+MINIO_ACCESS_KEY=1234
+MINIO_SECRET_KEY=123456789
+VMCK_API_URL=http://10.66.60.1:10000/v0/
+DEBUG=True
+HOSTNAME=*
+ACS_INTERFACE_ADDRESS=http://10.66.60.1:8000/
+MANAGER_TAG=vagrant-pytest" > .env
+
+exec pipenv run pytest --liveserver 10.66.60.1:8000
+rm .env
--- testsuite/test_interface.py
+++ testsuite/test_interface.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 
 import pytest
 from django.conf import settings
@@ -9,15 +10,22 @@ import interface.backend.minio_api as storage
 from interface.models import Course, Submission
 
 
-@pytest.mark.skip(reason="Minio is not set up correctly")
 @pytest.mark.django_db
-def test_submission(client):
+def test_submission(client, live_server):
     filepath = settings.BASE_DIR / 'testsuite' / 'test.zip'
 
     User.objects.create_user('user', password='pw')
     client.login(username='user', password='pw')
     pc = Course.objects.create(name='PC', code='pc')
-    pc.assignment_set.create(code='a0')
+    pc.assignment_set.create(
+        code='a0',
+        name='a0',
+        max_score=100,
+        deadline_soft=datetime(2100, 1, 1),
+        deadline_hard=datetime(2100, 1, 1),
+        repo_url='https://github.com/vmck/assignment',
+        repo_branch='pc-00',
+    )
 
     with open(filepath, 'rb') as file:
         upload = SimpleUploadedFile(filepath.name,
@@ -30,16 +38,24 @@ def test_submission(client):
         )
 
     assert len(Submission.objects.all()) == 1
-    assert storage.exists('1.zip')
+    assert storage.exists('1.zip') == True
 
     submission = Submission.objects.all()[0]
 
     assert submission.state == submission.STATE_NEW
-    assert submission.assignment.code == 'pc-a0'
+    assert submission.assignment.code == 'a0'
     assert submission.archive_size > 0
     assert submission.vmck_job_id > 0
 
+    count = 0
     while submission.state != submission.STATE_DONE:
-        time.sleep(2)
+        submission = Submission.objects.all()[0]
 
-    assert submission.score > 0
+        time.sleep(10)
+
+        count += 1
+        if count == 50:
+            assert False == True
+
+    assert submission.score == 100
+    assert submission.total_score == 100
