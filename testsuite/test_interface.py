d75071accc88beb7bc0e3a6a2ebbc9889e28abad
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2019-12-16 00:46:18 +0200

    Add review check and remove warnings

--- testsuite/test_interface.py
+++ testsuite/test_interface.py
@@ -1,5 +1,5 @@
 import time
-from datetime import datetime
+from datetime import datetime, timezone
 
 import pytest
 from django.conf import settings
@@ -14,15 +14,15 @@ from interface.models import Course, Submission
 def test_submission(client, live_server):
     filepath = settings.BASE_DIR / 'testsuite' / 'test.zip'
 
-    User.objects.create_user('user', password='pw')
+    User.objects.create_user('user', password='pw', is_staff=True)
     client.login(username='user', password='pw')
     pc = Course.objects.create(name='PC', code='pc')
     pc.assignment_set.create(
         code='a0',
         name='a0',
         max_score=100,
-        deadline_soft=datetime(2100, 1, 1),
-        deadline_hard=datetime(2100, 1, 1),
+        deadline_soft=datetime(2100, 1, 1, tzinfo=timezone.utc),
+        deadline_hard=datetime(2100, 1, 1, tzinfo=timezone.utc),
         repo_url='https://github.com/vmck/assignment',
         repo_branch='pc-00',
     )
@@ -54,8 +54,19 @@ def test_submission(client, live_server):
         time.sleep(10)
 
         count += 1
-        if count == 50:
+        if count == 18:
             assert 0 == 1
 
     assert submission.score == 100
     assert submission.total_score == 100
+
+    client.post(
+        '/submission/1/review',
+        data={'review-code': '+10.0: Good Job'},
+        HTTP_REFERER='/'
+    )
+
+    submission = Submission.objects.all()[0]
+
+    assert len(submission.review_message) > 0
+    assert submission.total_score == 110
