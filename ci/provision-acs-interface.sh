7d2209b7736abab7c58e056784b32eb151645929
Alex Morega <alex@grep.ro>
2019-12-16 11:58:44 +0200

    Provision as root so that docker works first time

--- ci/provision-acs-interface.sh
+++ ci/provision-acs-interface.sh
@@ -1,22 +1,22 @@
 #!/bin/bash -e
 
-sudo apt-get update -qq
-sudo apt-get -qq install python3-pip libsasl2-dev python-dev libldap2-dev libssl-dev fortune
-sudo pip3 install pipenv
+apt-get update -qq
+apt-get -qq install python3-pip libsasl2-dev python-dev libldap2-dev libssl-dev fortune
+pip3 install pipenv
 
 
 cd /vagrant
-pipenv install --ignore-pipfile 2> /dev/null
-mkdir -p data
+sudo -Hu vagrant pipenv install --ignore-pipfile 2> /dev/null
+sudo -Hu vagrant mkdir -p data
 
 container=$(docker ps -f name=minio -aq)
 if [ -z $container ]; then (
   pipenv run examples/minio.sh
 ) fi
 
-pipenv run ./manage.py migrate
-pipenv run ./manage.py storage_setup
-pipenv run ./manage.py collectstatic
-pipenv run ./manage.py loaddata ci/fixtures.json
+sudo -Hu vagrant pipenv run ./manage.py migrate
+sudo -Hu vagrant pipenv run ./manage.py storage_setup
+sudo -Hu vagrant pipenv run ./manage.py collectstatic --noinput
+sudo -Hu vagrant pipenv run ./manage.py loaddata ci/fixtures.json
 
 echo "✔ acs-interface installed successfully"
--- ci/provision-cluster.sh
+++ ci/provision-cluster.sh
@@ -8,13 +8,13 @@ fi
 (
   set -x
   cd /opt
-  sudo chown $(whoami) .
   git clone https://github.com/liquidinvestigations/cluster
   cd /opt/cluster
   cp examples/cluster.ini ./
   sed -i '/nomad_meta/a vmck_worker = true' cluster.ini
   sed -i '/^run_jobs =.*/c\run_jobs = dnsmasq' cluster.ini
-  bin/docker.sh --rm --pull
+  chown -R vagrant: .
+  sudo -u vagrant bin/docker.sh --rm --pull
   docker exec cluster ./cluster.py wait
 )
 
