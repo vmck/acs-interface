c3e2af24a16fffe1184b096265f7faa7ee45a853
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-03-20 17:09:45 +0200

    Possible fix

--- interface/settings.py
+++ interface/settings.py
@@ -42,6 +42,9 @@ AUTH_LDAP_USER_SEARCH = LDAPSearch(
     ldap.SCOPE_SUBTREE,
     os.getenv('LDAP_USER_FILTER'),
 )
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
+    "ou=People,dc=curs,dc=pub,dc=ro", ldap.SCOPE_SUBTREE, "(objectClass=person)"
+)
 AUTH_LDAP_USER_ATTR_MAP = {
     'first_name': 'givenName',
     'last_name': 'sn',
