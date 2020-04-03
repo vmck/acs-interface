d46e0f413bb92bedd06973438af71491debb292f
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-04-03 19:16:53 +0300

    Validate the uploaded file

--- interface/backend/submission.py
+++ interface/backend/submission.py
@@ -1,17 +1,27 @@
 import logging
+from zipfile import ZipFile
 
 from django.db import transaction
 from django.utils import timezone
+from django.contrib import messages
 
-import interface.backend.minio_api as storage
 from interface.models import Assignment
+import interface.backend.minio_api as storage
 
 
 log = logging.getLogger(__name__)
 
 
+class CorruptZipFile(Exception):
+    pass
+
+
 def handle_submission(file, assignment, user):
     log.debug(f'Submission {file.name} received')
+
+    with ZipFile(file) as archive:
+        if archive.testzip():
+            raise CorruptZipFile()
 
     with transaction.atomic():
         """
