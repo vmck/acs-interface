62c3b924460d934b39a4830438d7751c7a7b24fd
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-04-10 17:13:10 +0300

    Buttons

--- interface/views.py
+++ interface/views.py
@@ -250,3 +250,13 @@ def subs_for_user(request, course_code, assignment_code, username):
         'assignment': assignment,
         'submissions': submissions,
     })
+
+@login_required
+def rerun_submission(request, pk):
+    submission = get_object_or_404(Submission, pk=pk)
+    submission.state = Submission.STATE_NEW
+    submission.evaluate()
+    from django.contrib.auth.models import Permission
+    perm_tuple = [(x.id, x.name) for x in Permission.objects.filter(user=request.user)]
+    print(perm_tuple)
+    return redirect(request.META['HTTP_REFERER'])
