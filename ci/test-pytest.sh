0cd18a6c9eb57ae1eec8bcbb94577333c1a58fb2
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-05-29 23:27:38 +0300

    Use the example .env

--- ci/test-pytest.sh
+++ ci/test-pytest.sh
@@ -4,14 +4,7 @@ cd /vagrant
 
 # set the necessary envs if they are not present
 if [ ! -f .env ]; then
-    echo "MINIO_ADDRESS=10.66.60.1:9000
-    MINIO_ACCESS_KEY=1234
-    MINIO_SECRET_KEY=123456789
-    VMCK_API_URL=http://10.66.60.1:10000/v0/
-    DEBUG=True
-    HOSTNAME=*
-    ACS_INTERFACE_ADDRESS=http://10.66.60.1:8000/
-    MANAGER_TAG=vagrant-pytest" > .env
+    cp ./examples/.env .
 fi
 
 exec pipenv run pytest --cov-report=xml --cov=interface --liveserver 10.66.60.1:8000 testsuite
