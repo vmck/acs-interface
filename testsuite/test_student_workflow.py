4ca186b75a3d749c9652104a32ba2a1741926bb4
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-05-31 00:07:22 +0300

    Remove skip

--- testsuite/test_student_workflow.py
+++ testsuite/test_student_workflow.py
@@ -13,7 +13,7 @@ from interface.models import Submission
 
 FILEPATH = settings.BASE_DIR / 'testsuite' / 'test.zip'
 
-@pytest.mark.skip
+
 @pytest.mark.django_db
 def test_submission(client, live_server, base_db_setup):
     FILEPATH = settings.BASE_DIR / 'testsuite' / 'test.zip'
