9010a30c2f738515032af9caf942a1052a5b3b7d
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-06-26 16:34:20 +0300

    Use different var

--- interface/uploadhandler.py
+++ interface/uploadhandler.py
@@ -4,14 +4,22 @@ from django.core.files.uploadhandler import MemoryFileUploadHandler, StopUpload
 
 
 class RestrictedFileUploadHandler(MemoryFileUploadHandler):
-    def handle_raw_input(self, input_data, META, content_length, boundary, encoding=None):
-        super().handle_raw_input(input_data, META, content_length, boundary, encoding=None)
+    def handle_raw_input(self, input_data, META, content_length,
+                         boundary, encoding=None):
+        super().handle_raw_input(
+            input_data,
+            META,
+            content_length,
+            boundary,
+            encoding=None,
+        )
 
+        self.too_big = False
         if content_length > settings.FILE_UPLOAD_MAX_MEMORY_SIZE:
-            self.activated = False
+            self.too_big = True
 
     def receive_data_chunk(self, raw_data, start):
-        if not self.activated:
+        if self.too_big:
             raise StopUpload((
                 'File must be below '
                 f'{filesizeformat(settings.FILE_UPLOAD_MAX_MEMORY_SIZE)}'
