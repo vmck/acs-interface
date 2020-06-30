e1738e5787ba491a1b12d07cea8a96ed770df4eb
Baruta Daniel Mihai <baruta.mihai@yahoo.com>
2020-06-30 15:51:20 +0300

    Limit filesize for upload (#230)
    * Limit filesize server-side
    
    * Use different var
    
    * Test filesize
    
    * Inform user about file size
    
    * Include tests
    
    * Cleaner approach
    
    * Flake8
    
    * Fix bad error

---
+++ interface/uploadhandler.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.files.uploadhandler import MemoryFileUploadHandler
+
+
+class RestrictedFileUploadHandler(MemoryFileUploadHandler):
+    def handle_raw_input(self, input_data, META, content_length,
+                         boundary, encoding=None):
+        super().handle_raw_input(
+            input_data,
+            META,
+            content_length,
+            boundary,
+            encoding=None,
+        )
+
+        if content_length >= settings.FILE_UPLOAD_MAX_MEMORY_SIZE:
+            return (
+                {'csrfmiddlewaretoken': META.get('CSRF_COOKIE', '')},
+                {'file': InMemoryUploadedFile(
+                            file=None,
+                            field_name=None,
+                            name='FileTooBig',
+                            content_type=None,
+                            size=content_length,
+                            charset=None,
+                            content_type_extra=None,
+                )},
+            )
