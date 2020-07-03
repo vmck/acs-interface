1bc2ad9c71e2f1297036d5687a25f7c8a08bf862
Bianca Necula <bianca.necula12@gmail.com>
2020-07-03 13:30:46 +0300

    Add user page for submissions

--- interface/views.py
+++ interface/views.py
@@ -296,3 +296,14 @@ def subs_for_user(request, course_pk, assignment_pk, username):
         'assignment': assignment,
         'submissions': submissions,
     })
+
+@login_required
+def user_page(request, username):
+    user = get_object_or_404(User, username=username)
+    submissions_aux = Submission.objects.all().order_by('-timestamp')
+    submissions = submissions_aux.filter(user=user)
+    if request.user.username != username:
+        log.warning(f"User attempted to access {username}")
+        raise Http404("You are not allowed to access this page.")
+    return render(request, 'interface/subs_for_user.html', { 'submissions': submissions })
+    
