a9034db2eaaa92af78720b7d508de33f51cb027c
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-08-22 17:53:09 +0300

    format files

--- interface/settings.py
+++ interface/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).parent.parent
 
 BLOCK_SIZE = 32 * 1024
 
-MEDIA_ROOT = 'interface/templates/interface/media'
-MEDIA_URL = '/media/'
+MEDIA_ROOT = "interface/templates/interface/media"
+MEDIA_URL = "/media/"
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -176,12 +176,11 @@ LOGGING = {
     },
 }
 
-SENTRY_DSN = os.environ.get('SENTRY_DSN')
+SENTRY_DSN = os.environ.get("SENTRY_DSN")
 sentry_sdk.init(
     dsn=SENTRY_DSN,
     integrations=[DjangoIntegration()],
-
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
+    send_default_pii=True,
 )
