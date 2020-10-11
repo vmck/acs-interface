563d0ed3750fcfb9af3818b5a2cdde22f34943ae
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-10-11 19:57:46 +0300

    improve postgres.sh

--- examples/postgres.sh
+++ examples/postgres.sh
@@ -2,7 +2,7 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"/..
 
-docker run --rm -p 5432:5432 --name database --detach \
+docker run --rm -p ${POSTGRES_PORT}:5432 --name ${POSTGRES_DB} --detach \
   --env "POSTGRES_DB=${POSTGRES_DB}" \
   --env "POSTGRES_USER=${POSTGRES_USER}" \
   --env "POSTGRES_PASSWORD=${POSTGRES_PASSWORD}" \
