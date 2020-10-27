f9e55ffb5cf17f416a4a01785b5786d3016ddd8c
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2020-10-27 16:41:44 +0200

    st

--- ci/test-pytest.sh
+++ ci/test-pytest.sh
@@ -7,6 +7,9 @@ fi
 # set the necessary envs if they are not present
 if [ ! -f .env ]; then
     cp ./examples/.env .
+
+    # disable profiling if testing
+    sed -i "/PROFILE=/c\PROFILE=False"
 fi
 
 exec sudo -Hu vagrant pipenv run pytest
