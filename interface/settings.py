ba69d333e3304ad5ab094a2096651f327c12ac41
George Muraru <murarugeorgec@gmail.com>
2020-10-28 19:24:20 +0200

    Add GZIP Middleware

--- interface/settings.py
+++ interface/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
+    "django.middleware.gzip.GZipMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
