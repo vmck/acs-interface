94d8f69a900e942b8353e7d7e52979eca129b04f
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2021-06-30 00:27:13 +0300

    Impose python version

--- ci/provision-acs-interface.sh
+++ ci/provision-acs-interface.sh
@@ -8,7 +8,8 @@ else
     sudo -Hu vagrant cp ./examples/.env .
 fi
 
-sudo -Hu vagrant pipenv install --dev --ignore-pipfile 2> /dev/null
+# In case the pyenv installs the correct version of python we need to input a `y`
+yes | sudo -Hu vagrant pipenv install --dev --ignore-pipfile 2> /dev/null
 sudo -Hu vagrant mkdir -p data
 
 container=$(docker ps -f name=minio -aq)
--- ci/provision-system.sh
+++ ci/provision-system.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
 
 sudo apt-get update -yqq
-sudo apt-get install -yqq python3-pip libsasl2-dev python-dev libldap2-dev libssl-dev fortune
+sudo apt-get install -yqq python3-pip libsasl2-dev python-dev \
+                        libldap2-dev libssl-dev libffi-dev fortune
 
 if [ -z "$CI" ]; then
     sudo apt-get install -yqq docker.io git
+    sudo -Hu vagrant curl https://pyenv.run | bash
 else
     sudo useradd -m vagrant
     sudo usermod -aG sudo vagrant
