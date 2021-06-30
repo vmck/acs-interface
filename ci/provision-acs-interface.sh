7821ed44783590c3981e295449f2f88d4ec68741
Baruta Daniel Mihail <baruta.mihai@yahoo.com>
2021-06-30 12:37:48 +0300

    Wip

--- ci/provision-acs-interface.sh
+++ ci/provision-acs-interface.sh
@@ -7,24 +7,24 @@ else
     sudo -Hu vagrant cp ./examples/.env .
 fi
 
-"$SUDO" pipenv install --dev --ignore-pipfile
-"$SUDO" mkdir -p data
+$SUDO pipenv install --dev --ignore-pipfile
+$SUDO mkdir -p data
 
 container=$(docker ps -f name=minio -aq)
 if [ -z "$container" ]; then (
-    "$SUDO" pipenv run examples/minio.sh
+    $SUDO pipenv run examples/minio.sh
 ) fi
 
 container=$(docker ps -f name=database -aq)
 if [ -z "$container" ]; then (
-    "$SUDO" pipenv run examples/postgres.sh
+    $SUDO pipenv run examples/postgres.sh
 ) fi
 
 sleep 2
 
-"$SUDO" pipenv run ./manage.py migrate
-"$SUDO" pipenv run ./manage.py storage_setup
-"$SUDO" pipenv run ./manage.py collectstatic --noinput
-"$SUDO" pipenv run ./manage.py loaddata ci/fixtures.json
+$SUDO pipenv run ./manage.py migrate
+$SUDO pipenv run ./manage.py storage_setup
+$SUDO pipenv run ./manage.py collectstatic --noinput
+$SUDO pipenv run ./manage.py loaddata ci/fixtures.json
 
 echo "✔ acs-interface installed successfully"
